3dcbadfce206e6b54ebe4bb48bd518e5ea21e603
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2025-02-23 18:55:34 +0100

    Improve settings page layout for time slot management; adjust styles and consolidate table structure

--- shooting-range-booking/admin/templates/settings-page.php
+++ shooting-range-booking/admin/templates/settings-page.php
@@ -75,41 +75,48 @@ if (!empty($time_slots)) {
                 </th>
             </tr>
             <tr>
-                <table id="time-slots-container">
-                    <thead>
-                        <tr>
-                            <th>Zakres godzin</th>
-                            <th>Rodzaj strzelania</th>
-                            <th>Akcje</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php if (!empty($time_slots)): ?>
-                            <?php foreach ($time_slots as $i => $slot): ?>
-                                <tr>
-                                    <td>
-                                        <input type="text" id="time_slots_<?php echo $i; ?>_range" name="time_slots[<?php echo $i; ?>][range]" value="<?php echo esc_attr($slot['range']); ?>" required>
-                                    </td>
-                                    <td>
-                                        <select id="time_slots_<?php echo $i; ?>_type" name="time_slots[<?php echo $i; ?>][type]" required>
-                                            <option value="static" <?php selected($slot['type'], 'static'); ?>>Statyczne</option>
-                                            <option value="dynamic" <?php selected($slot['type'], 'dynamic'); ?>>Dynamiczne</option>
-                                        </select>
-                                    </td>
-                                    <td>
-                                        <button type="button" class="button remove-time-slot">Usuń</button>
-                                    </td>
-                                </tr>
-                            <?php endforeach; ?>
-                        <?php endif; ?>
-                    </tbody>
-                </table>
+                <th colspan="2">
+                    <table id="time-slots-container">
+                        <thead>
+                            <tr>
+                                <th>Zakres godzin</th>
+                                <th>Rodzaj strzelania</th>
+                                <th>Akcje</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php if (!empty($time_slots)): ?>
+                                <?php foreach ($time_slots as $i => $slot): ?>
+                                    <tr>
+                                        <td>
+                                            <input type="text" id="time_slots_<?php echo $i; ?>_range" name="time_slots[<?php echo $i; ?>][range]" value="<?php echo esc_attr($slot['range']); ?>" required>
+                                        </td>
+                                        <td>
+                                            <select id="time_slots_<?php echo $i; ?>_type" name="time_slots[<?php echo $i; ?>][type]" required>
+                                                <option value="static" <?php selected($slot['type'], 'static'); ?>>Statyczne</option>
+                                                <option value="dynamic" <?php selected($slot['type'], 'dynamic'); ?>>Dynamiczne</option>
+                                            </select>
+                                        </td>
+                                        <td>
+                                            <button type="button" class="button remove-time-slot">Usuń</button>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            <?php endif; ?>
+                        </tbody>
+                    </table>
+                </th>
+            </tr>
+            <tr>
+                <th colspan="2">
+                    <button type="button" class="button" id="add-time-slot">Dodaj slot czasowy</button>
+                </th>
+            </tr>
+            <tr>
+                <th colspan="2">
+                    <input type="submit" class="button-primary" value="Zapisz ustawienia">
+                </th>
             </tr>
         </table>
-        <button type="button" class="button" id="add-time-slot">Dodaj slot czasowy</button>
-
-        <p class="submit">
-            <input type="submit" class="button-primary" value="Zapisz ustawienia">
-        </p>
     </form>
 </div>
