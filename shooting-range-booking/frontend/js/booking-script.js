c7d7d0d01616db5eec5f22be4b509d2b303401f5
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2025-02-23 17:48:38 +0100

    Implement dynamic time slot management in admin script; add functionality to add and remove time slots

--- shooting-range-booking/frontend/js/booking-script.js
+++ shooting-range-booking/frontend/js/booking-script.js
@@ -77,35 +77,6 @@ jQuery(document).ready(function ($) {
                 }
             });
         });
-
-        let timeSlotIndex = srbs_ajax.timeSlotIndex || 0;
-
-        $('#add-time-slot').on('click', function () {
-            const container = $('#time-slots-container');
-            const newRow = `
-            <tr>
-                <th><label for="time_slots_${timeSlotIndex}_range">Zakres godzin:</label></th>
-                <td><input type="text" id="time_slots_${timeSlotIndex}_range" name="time_slots[${timeSlotIndex}][range]" required></td>
-            </tr>
-            <tr>
-                <th><label for="time_slots_${timeSlotIndex}_type">Rodzaj strzelania:</label></th>
-                <td>
-                    <select id="time_slots_${timeSlotIndex}_type" name="time_slots[${timeSlotIndex}][type]" required>
-                        <option value="static">Statyczne</option>
-                        <option value="dynamic">Dynamiczne</option>
-                    </select>
-                    <button type="button" class="button remove-time-slot">Usuń</button>
-                </td>
-            </tr>
-        `;
-            container.append(newRow);
-            timeSlotIndex++;
-        });
-
-        $('#time-slots-container').on('click', '.remove-time-slot', function () {
-            $(this).closest('tr').next().remove();
-            $(this).closest('tr').remove();
-        });
     }
 
     function showModal(message) {
